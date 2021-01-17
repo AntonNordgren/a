0584031fd432a6c21d6ed35ed5d6599fd3bdabfd
Anton Nordgren <anton.nordgren1@gmail.com>
2021-01-17 18:35:30 +0100

    qweasdzxc

--- components/TopicContent.js
+++ components/TopicContent.js
@@ -11,9 +11,9 @@ export default function TopicContent({ data }) {
       <Row>
         {
           data.map(shoe => (
-            <Col className="p-2" sm={6} md={4} lg={3}>
+            <Col className="p-2" xs={12} sm={6} md={4} lg={3}>
               <Card style={{ borderRadius: "0px", border: "none", boxShadow: "gray 5px 5px 10px", borderRadius: "0px" }}>
-                <Card.Img variant="top" style={{ margin: "auto", padding: "0px", borderRadius: "0px", width: "100%", height: "15vw", objectFit: "cover" }} src={shoe.img} />
+                <Card.Img variant="top" style={{ margin: "auto", padding: "0px", borderRadius: "0px", width: "100%", maxHeight: "260px", objectFit: "cover" }} src={shoe.img} />
                 <Card.Body>
                   <Card.Title><h3>{shoe.name}</h3></Card.Title>
                   <Card.Text>

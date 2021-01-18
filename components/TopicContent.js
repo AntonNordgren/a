1dba7c80504227e5cfa7c600d37399551606d37a
Anton Nordgren <anton.nordgren1@gmail.com>
2021-01-18 16:12:03 +0100

    Added som test text

--- components/TopicContent.js
+++ components/TopicContent.js
@@ -7,25 +7,27 @@ import Col from 'react-bootstrap/Col'
 export default function TopicContent({ data }) {
 
   return (
-    <Container>
-      <Row>
-        {
-          data.map(shoe => (
-            <Col className="p-2" xs={12} sm={6} md={4} lg={3}>
-              <Card style={{ borderRadius: "0px", border: "none", boxShadow: "gray 5px 5px 10px", borderRadius: "0px" }}>
-                <Card.Img variant="top" style={{ margin: "auto", padding: "0px", borderRadius: "0px", width: "100%", height: "260px", objectFit: "cover" }} src={shoe.img} />
-                <Card.Body>
-                  <Card.Title><h3>{shoe.name}</h3></Card.Title>
-                  <Card.Text>
-                    {shoe.text}
-                  </Card.Text>
-                  <Button variant="primary">Köp här!</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))
-        }
-      </Row>
-    </Container>
+    <div>
+      <Container>
+        <Row>
+          {
+            data.map(shoe => (
+              <Col className="p-2" xs={12} sm={6} md={4} lg={4}>
+                <Card style={{ borderRadius: "0px", border: "none", boxShadow: "gray 2px 2px 5px", borderRadius: "0px" }}>
+                  <Card.Img variant="top" style={{ margin: "auto", padding: "0px", borderRadius: "0px", width: "100%", height: "320px", objectFit: "cover" }} src={shoe.img} />
+                  <Card.Body>
+                    <Card.Title><h4>{shoe.name}</h4></Card.Title>
+                    <Card.Text>
+                      {shoe.text}
+                    </Card.Text>
+                    <Button variant="secondary" style={{ borderRadius: "0px", boxShadow: "gray 2px 2px 5px" }}>Köp här!</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,8 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora:wght@500&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet"/> 
       </Head>
       <Layout>
         <Component {...pageProps} />

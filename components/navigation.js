33c4804fcd69d3cc5ba6bcdc9380f6a700a9a5d2
Anton Nordgren <anton.nordgren1@gmail.com>
2021-03-02 19:32:21 +0100

    asd

--- components/navigation.js
+++ components/navigation.js
@@ -71,7 +71,9 @@ export default function Navigation() {
         return (
           <div className="navigation-link navigation-link-normal">
             <Link href={nav.link} key={nav.name}>
-              <a className="">{nav.name}</a>
+              <div className="navigation-link-normal-container">
+                {nav.name}
+              </div>
             </Link>
           </div>
         )
@@ -92,7 +94,10 @@ export default function Navigation() {
     return(
       <div className={result}>
 
+
         <div className="dropdown-header">
+
+          
           <svg className="dropdownIcon" onClick={() => toggleDropdown()} xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
           </svg>
@@ -102,6 +107,7 @@ export default function Navigation() {
           { navLinks}
         </div>
 
+        <img className="dropdown-logo" src="TG_TANGENTBORDSGUIDEN.png"></img>
       </div>
     )
   }

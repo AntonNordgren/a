5650478a42faddf2e7d0db4749c3c4ac24abbf47
Anton Nordgren <anton.nordgren1@gmail.com>
2021-03-31 21:20:12 +0200

    !

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,31 @@ export default function Home() {
 
   return (
     <Container>
-      <div style={{textAlign: "center", marginTop: "20px"}}>
-        <img src="tangentbordsguiden.png" style={{width: "100%"}}/>
-        <h1 style={{marginTop: "20px"}}>Under konstruktion</h1>
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <img src="/TG_Tangentbordsguiden_MB.png" style={{ width: "100%" }} />
+
+        <h1>Under Utveckling</h1>
+
+        <Carousel controls={false} interval={3000} indicators={false}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://azcd.harveynorman.com.au/media/catalog/product/cache/21/image/992x558/9df78eab33525d08d6e5fb8d27136e95/r/z/rz03-03390200-razer-huntsman-mini-gaming-keyboard.jpg"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://venturebeat.com/wp-content/uploads/2020/05/logitech-2.jpg?w=1200&strip=all"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://images-na.ssl-images-amazon.com/images/I/71ZLaIN981L._AC_SL1500_.jpg"
+            />
+          </Carousel.Item>
+        </Carousel>
       </div>
     </Container>
   )

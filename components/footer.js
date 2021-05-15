d5dc8ca17207b79bf7be6a4e0e90833817fa0337
Anton Nordgren <anton.nordgren1@gmail.com>
2021-05-15 22:21:53 +0200

    qwe

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,17 @@
 import Navbar from 'react-bootstrap/Navbar'
 
 export default function Footer() {
-  const style = {
-    backgroundColor: "#1f2833",
-    height: "80px",
-    color: "#ffffff5d",
-    textAlign: "center"
+
+  function topFunction() {
+    window.document.body.scrollTop = 0
+    window.document.documentElement.scrollTop = 0
   }
 
   return (
-    <Navbar className="p-0 m-0" style={style}>
-      <div style={{width: "100%"}}>
-        <object data="/TG_Turkos.svg" type="image/svg+xml" width="250px"></object>
-        {/* <object data="yours.svg" type="image/svg+xml"></object> */}
+    <Navbar className="p-0 m-0 footer">
+      <div className="footer-logo-wrapper" onClick={ () => topFunction() }>
+        <object style={{ pointerEvents: "none" }} data="/TG_Turkos.svg" type="image/svg+xml" width="250px" />
       </div>
     </Navbar>
   )
-}
+} 
